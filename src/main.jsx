1231f4d4c60d1e856c2e45fbec0e03a564cdf44a
Florinho34 <florinho34@googlemail.com>
2026-03-10 13:00:11 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PersonalityTest from './PersonalityTest.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PersonalityTest />
+  </React.StrictMode>,
+)
