d121d3d368b2ef96b0173033dafea9870cc3cf42
Florinho34 <florinho34@googlemail.com>
2026-06-10 21:36:19 +0200

    Add ConsentBanner component to main rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PersonalityTest from './PersonalityTest.jsx'
+import ConsentBanner from './components/ConsentBanner'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersonalityTest />
+    <ConsentBanner privacyUrl="https://florian-lingner.ch/datenschutz" />
   </React.StrictMode>,
 )
